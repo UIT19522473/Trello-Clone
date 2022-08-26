84271f33b8dc44a47fd4e8c98d41859d48b02b0f
UIT19522473 <71434447+UIT19522473@users.noreply.github.com>
2022-08-26 17:23:10 +0700

    complete b2

--- my-trello/src/App.js
+++ my-trello/src/App.js
@@ -1,9 +1,118 @@
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div className="trello-container">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://1.bp.blogspot.com/-GcV8T-MADlk/XqhWymNhSlI/AAAAAAAAjq4/TmPfMXosBPgS2whGvZTm6JwlCRiE_s-gwCLcBGAsYHQ/s1600/Anh-gai-xinh-toc-ngan%2B%25286%2529.jpg"
+                alt="logo"
+              />
+              Title: hello every one
+            </li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://1.bp.blogspot.com/-GcV8T-MADlk/XqhWymNhSlI/AAAAAAAAjq4/TmPfMXosBPgS2whGvZTm6JwlCRiE_s-gwCLcBGAsYHQ/s1600/Anh-gai-xinh-toc-ngan%2B%25286%2529.jpg"
+                alt="logo"
+              />
+              Title: hello every one
+            </li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://1.bp.blogspot.com/-GcV8T-MADlk/XqhWymNhSlI/AAAAAAAAjq4/TmPfMXosBPgS2whGvZTm6JwlCRiE_s-gwCLcBGAsYHQ/s1600/Anh-gai-xinh-toc-ngan%2B%25286%2529.jpg"
+                alt="logo"
+              />
+              Title: hello every one
+            </li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://1.bp.blogspot.com/-GcV8T-MADlk/XqhWymNhSlI/AAAAAAAAjq4/TmPfMXosBPgS2whGvZTm6JwlCRiE_s-gwCLcBGAsYHQ/s1600/Anh-gai-xinh-toc-ngan%2B%25286%2529.jpg"
+                alt="logo"
+              />
+              Title: hello every one
+            </li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://1.bp.blogspot.com/-GcV8T-MADlk/XqhWymNhSlI/AAAAAAAAjq4/TmPfMXosBPgS2whGvZTm6JwlCRiE_s-gwCLcBGAsYHQ/s1600/Anh-gai-xinh-toc-ngan%2B%25286%2529.jpg"
+                alt="logo"
+              />
+              Title: hello every one
+            </li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className="column">
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src="https://1.bp.blogspot.com/-GcV8T-MADlk/XqhWymNhSlI/AAAAAAAAjq4/TmPfMXosBPgS2whGvZTm6JwlCRiE_s-gwCLcBGAsYHQ/s1600/Anh-gai-xinh-toc-ngan%2B%25286%2529.jpg"
+                alt="logo"
+              />
+              Title: hello every one
+            </li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+            <li>what is this</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+      </div>
     </div>
   );
 }
